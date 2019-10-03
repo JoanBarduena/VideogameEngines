3330cbcdcc8c514954b93401dd7931b659b5365a
Lidux <36154195+JoanBarduena@users.noreply.github.com>
2019-10-03 11:33:50 +0200

    Grid implemented again

--- Engine/ModuleSceneIntro.cpp
+++ Engine/ModuleSceneIntro.cpp
@@ -1,6 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
+#include "Primitive.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -14,6 +15,9 @@ bool ModuleSceneIntro::Start()
 {
 	LOG("Loading Intro assets");
 	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
 
 	return ret;
 }
@@ -29,5 +33,9 @@ bool ModuleSceneIntro::CleanUp()
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
 	return UPDATE_CONTINUE;
 }
