977a8125afc6f2c197174702890a13acf9741e5e
JoanBarduena <36154195+JoanBarduena@users.noreply.github.com>
2019-10-28 00:15:23 +0100

    Solved camera bug while merge

--- Engine/ModuleCamera3D.cpp
+++ Engine/ModuleCamera3D.cpp
@@ -41,105 +41,86 @@ bool ModuleCamera3D::CleanUp()
 // -----------------------------------------------------------------
 update_status ModuleCamera3D::Update(float dt)
 {
-	//if (game_active)
-	
 	vec3 newPos(0, 0, 0);
 	float speed = 3.0f * dt;
 	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-			speed = 8.0f * dt;
-	speed = Sensitivity * dt;
-	vec3 newPos(0, 0, 0);
-	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-		speed *= 2;
+		speed = 8.0f * dt;
 
-		if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
-		if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
+	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
+	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
 
-		if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
-		if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
+	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
+	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
 
-		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
-		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
 
-		Position += newPos;
-		Reference += newPos;
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
 
-		// Mouse motion ----------------
-		float Sensitivity = 0.25f;
+	Position += newPos;
+	Reference += newPos;
+
 	// Mouse motion ----------------
+	float Sensitivity = 0.25f;
 
-		if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-		{
-			int dx = -App->input->GetMouseXMotion();
-			int dy = -App->input->GetMouseYMotion();
+	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+	{
+		int dx = -App->input->GetMouseXMotion();
+		int dy = -App->input->GetMouseYMotion();
 
 
-			Position -= Reference;
+		Position -= Reference;
 
-			if (dx != 0)
-			{
-				float DeltaX = (float)dx * Sensitivity;
 		if (dx != 0)
 		{
-			float DeltaX = (float)dx * speed;
+			float DeltaX = (float)dx * Sensitivity;
 
-				X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-				Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-				Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			}
+			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+		}
 
-			if (dy != 0)
-			{
-				float DeltaY = (float)dy * Sensitivity;
 		if (dy != 0)
 		{
-			float DeltaY = (float)dy * speed;
+			float DeltaY = (float)dy * Sensitivity;
 
-				Y = rotate(Y, DeltaY, X);
-				Z = rotate(Z, DeltaY, X);
+			Y = rotate(Y, DeltaY, X);
+			Z = rotate(Z, DeltaY, X);
 
-				if (Y.y < 0.0f)
-				{
-					Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
-					Y = cross(Z, X);
-				}
+			if (Y.y < 0.0f)
+			{
+				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
+				Y = cross(Z, X);
 			}
-
-			Position = Reference + Z * length(Position);
 		}
-		// Zoom
-		if (App->input->GetMouseZ() != 0)
-		{
-			vec3 vec = (0, 0, 0);
-			vec3 distance = Position - Reference;
 
-			if (App->input->GetMouseZ() > 0)
-			{
-				vec -= Z * Sensitivity;
-			}
+		Position = Reference + Z * length(Position);
+	}
 
-			if (App->input->GetMouseZ() < 0)
-			{
-				vec += Z * Sensitivity;
-			}
+	// Zoom
+	if (App->input->GetMouseZ() != 0)
+	{
+		vec3 vec = (0, 0, 0);
+		vec3 distance = Position - Reference;
 
-			Position += vec;
 		if (App->input->GetMouseZ() > 0)
 		{
-			vec -= Z * speed;
+			vec -= Z * Sensitivity;
 		}
 
 		if (App->input->GetMouseZ() < 0)
 		{
-			vec += Z * speed;
+			vec += Z * Sensitivity;
 		}
 
-		// Camera Reset
+		Position += vec;
+	}
 
-		if (App->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
-		{
-			ResetCamera();
-		}
+	// Camera Reset
+
+	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
+	{
+		ResetCamera();
+	}
 
 	// Recalculate matrix -------------
 	CalculateViewMatrix();
